06726544ef95c235d2b261738cb3d7348c029fd7
YuWakui <wak121.y@gmail.com>
2024-01-03 17:01:50 +0900

    Initial Commit

---
+++ CoreBluetoothSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoreBluetoothSample
+//
+//  Created by 和久井侑 on 2024/01/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CoreBluetoothSample/CoreBluetoothSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBluetoothSampleApp.swift
+//  CoreBluetoothSample
+//
+//  Created by 和久井侑 on 2024/01/03.
+//
+
+import SwiftUI
+
+@main
+struct CoreBluetoothSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
